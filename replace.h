8c5e5846006c1a5fbc6eb072378349f5de99507d
nmke-de <github@nmke.de>
2022-10-19 15:09:45 +0200

    Added substring type

--- replace.h
+++ replace.h
@@ -1,2 +1,13 @@
 #pragma once
 char readc(int);
+
+typedef struct {
+	const char *pattern;
+	const int len;
+	char *buf;
+	int ptr;
+} substring;
+
+// This init macro requires string.h and alloca.h
+#define subpattern(str) \
+	(substring) { (str), strlen((str)), alloca(sizeof(char) * strlen((str))), 0 }
