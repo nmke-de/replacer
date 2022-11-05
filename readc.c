e42021696b8f9d0a641ec80d7c9f9c849ce5040e
nmke-de <github@nmke.de>
2022-11-05 18:55:08 +0100

    Reimplemented readc()

--- readc.c
+++ readc.c
@@ -1,12 +1,19 @@
 #include <unistd.h>
 #include "replace.h"
 
+#define BUFLEN 512
+
 /*
 read a single character from file with file descriptor fd. If no characters could be read, then return -1.
 */
 char readc(int fd) {
-	static char c[2];
-	if (read(fd, c, 1) < 1)
+	static char c[BUFLEN];
+	static int ptr = BUFLEN;
+	static int rl = BUFLEN;
+	if (rl == ptr) {
+		rl = read(fd, c, BUFLEN);
+		ptr = 0;
+	} else if (rl < 1)
 		return -1;
-	return *c;
+	return c[ptr++];
 }
